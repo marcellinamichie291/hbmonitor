c770e96db7e8bd229d58a0fe5402b43a8bd213da
taeheekim <taeheehouse@gmail.com>
2022-07-22 09:00:30 +0900

    PnL Score added

--- pages/page2_bitcoin.py
+++ pages/page2_bitcoin.py
@@ -9,9 +9,11 @@ import requests
 access_token=st.secrets['cryptoquant']
 headers = {'Authorization': 'Bearer ' + access_token}
 
+
+symbol=['mvrv','sopr_ratio']
 url={'mvrv':'https://api.cryptoquant.com/v1/btc/market-indicator/mvrv?window=day&limit=2000&from=',
      'sopr_ratio':'https://api.cryptoquant.com/v1/btc/market-indicator/sopr-ratio?window=day&limit=2000&from=',
-     'nupl':'https://api.cryptoquant.com/v1/btc/network-indicator/nupl?window=day&limit=2000&from=',
+#     'nupl':'https://api.cryptoquant.com/v1/btc/network-indicator/nupl?window=day&limit=2000&from=',
 #     'oi':'https://api.cryptoquant.com/v1/btc/market-data/open-interest?window=day&limit=2000&exchange=all_exchange&from=',
 #     'mv':'https://api.cryptoquant.com/v1/btc/market-data/capitalization?window=day&limit=2000&from=',
 #     'est_lv':'https://api.cryptoquant.com/v1/btc/market-indicator/estimated-leverage-ratio?exchange=binance&window=day&limit=2000&from=',
@@ -36,23 +38,34 @@ def fetch_data(type,df_day):
 
 st.title("Page2 : Bitcoin Monitoring")
 st.header("1.Onchain(PnL) Monitoring")
-symbol=['mvrv','sopr_ratio','nupl']
+
 result={}
 for i in symbol:
     df=fetch_data(i,365*5)
     df['rank']=df[i].rank(ascending=False)
-    value=df[i][-1]
+    t0=df[i][-1]
+    t_1=df[i][-2]
+    diff=t0-t_1
+
     rank=100*df['rank'][-1]/len(df)
 
-
-
     tmp={
-        'VALUE' : '{:,.2f}'.format(value),
-        '5Y_PERCENTILE' : '{:,.0f}%'.format(rank),
+        'VALUE' : '{:,.2f}'.format(t0),
+        'DIFF' : '{:,.2f}'.format(diff),
+        'SCORE': '{:,.0f}'.format(rank),
     }
     result[i]=tmp
 
-st.table(pd.DataFrame(result).transpose())
+result=pd.DataFrame(result).transpose()
+#Col1
+col1, col2, col3 = st.columns(3)
+col1.metric("score",result['SCORE'].astype(int).mean())
+col2.metric(symbol[0],result.loc[symbol[0],'VALUE'],result.loc[symbol[0],'DIFF'])
+col3.metric(symbol[1],result.loc[symbol[1],'VALUE'],result.loc[symbol[1],'DIFF'])
+
+
+
+
 
 
 
