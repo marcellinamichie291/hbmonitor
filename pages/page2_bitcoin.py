2c0c3ad4ae58563980ad90bf724307f65070a431
taeheekim <taeheehouse@gmail.com>
2022-07-31 16:32:51 +0900

    funding_rates added to BTC derivatives score

--- pages/page2_bitcoin.py
+++ pages/page2_bitcoin.py
@@ -9,13 +9,13 @@ import requests
 access_token=st.secrets['cryptoquant']
 headers = {'Authorization': 'Bearer ' + access_token}
 
+
 url={'mvrv' : 'https://api.cryptoquant.com/v1/btc/market-indicator/mvrv?window=day&limit=2000&from=',
      'sopr_ratio' : 'https://api.cryptoquant.com/v1/btc/market-indicator/sopr-ratio?window=day&limit=2000&from=',
      'puell_multiple' : 'https://api.cryptoquant.com/v1/btc/network-indicator/puell-multiple?window=day&limit=2000&from=',
-     'open_interest':'https://api.cryptoquant.com/v1/btc/market-data/open-interest?window=day&limit=2000&exchange=all_exchange&from=',
      'pnl_utxo':'https://api.cryptoquant.com/v1/btc/network-indicator/pnl-utxo?window=day&limit=2000&from=',
-#     'mv':'https://api.cryptoquant.com/v1/btc/market-data/capitalization?window=day&limit=2000&from=',
-#     'estimated_leverage_ratio':'https://api.cryptoquant.com/v1/btc/market-indicator/estimated-leverage-ratio?exchange=binance&window=day&limit=2000&from=',
+     'open_interest': 'https://api.cryptoquant.com/v1/btc/market-data/open-interest?window=day&limit=2000&exchange=all_exchange&from=',
+     'funding_rates': 'https://api.cryptoquant.com/v1/btc/market-data/funding-rates?window=day&limit=2000&exchange=all_exchange&from=',
 
     }
 
@@ -68,7 +68,6 @@ for i in symbol:
     result[i]=tmp
 
 result=pd.DataFrame(result).transpose()
-#Col1
 
 col1, col2, col3, col4, col5 = st.columns(5)
 col1.metric("score",result['SCORE'].astype(float).mean().astype(int))
@@ -77,19 +76,45 @@ col3.metric(symbol[1],result.loc[symbol[1],'VALUE'],result.loc[symbol[1],'DIFF']
 col4.metric(symbol[2],result.loc[symbol[2],'VALUE'],result.loc[symbol[2],'DIFF'])
 col5.metric('utxo in profit(%)',result.loc[symbol[3],'VALUE'],result.loc[symbol[3],'DIFF'])
 
-#Leverage
-st.header("2.Leverage Score") #the higher, the higher risk
-symbol=['open_interest']
-df=fetch_data(symbol[0],365*5)
-df_rsi=pd.DataFrame(RSIIndicator(df['open_interest']).rsi().dropna())
-df_rsi['rank']=df_rsi['rsi'].rank(ascending=False)
-rank=100*df_rsi['rank'][-1]/len(df_rsi)
-p0='{:,.2f}'.format(df_rsi['rsi'][-1])
-diff='{:,.2f}'.format(df_rsi['rsi'][-1]-df_rsi['rsi'][-2])
 
-col1, col2 = st.columns(2)
-col1.metric("score",int(rank))
-col2.metric('open_interest_RSI',p0,diff)
+#2.Derivatives
+st.header("2.Derivatives") #the higher, the higher risk
+symbol=['open_interest','funding_rates']
+
+
+result={}
+for i in symbol:
+    df = fetch_data(i, 365 * 5)
+    if i==symbol[0]:
+
+        df = pd.DataFrame(RSIIndicator(df['open_interest']).rsi().dropna())
+        df['rank'] = df['rsi'].rank(ascending=False)
+        rank = 100 * df['rank'][-1] / len(df)
+        t0 = df['rsi'][-1]
+        t_1 = df['rsi'][-2]
+        diff = t0-t_1
+
+    else:
+
+        df=df*100
+        df['rank'] = df[i].rank(ascending=False) #the lower index, ther higher score
+        rank = 100 * df['rank'][-1] / len(df)
+        t0=df[i][-1]
+        t_1=df[i][-2]
+        diff=t0-t_1
+    tmp={
+        'VALUE' : '{:,.2f}'.format(t0),
+        'DIFF' : '{:,.2f}'.format(diff),
+        'SCORE': '{:,.0f}'.format(rank),
+    }
+    result[i]=tmp
+
+result=pd.DataFrame(result).transpose()
+
+col1, col2, col3 = st.columns(3)
+col1.metric("score",result['SCORE'].astype(float).mean().astype(int))
+col2.metric("Open interest RSI(14d)",result.loc[symbol[0],'VALUE'],result.loc[symbol[0],'DIFF'])
+col3.metric("Funding rates(PERP,8h,bp)",result.loc[symbol[1],'VALUE'],result.loc[symbol[1],'DIFF'])
 
 
 
