90b19ecdab2be15566f241e9419cfd2d175ec1b7
taeheekim <taeheehouse@gmail.com>
2022-08-02 17:57:10 +0900

    Upbit Disclosure added

--- main_page.py
+++ main_page.py
@@ -1,24 +1,16 @@
 import streamlit as st
 import pandas as pd
+
 #dynamoDB
 import boto3
 from boto3.dynamodb.conditions import Key
 service='dynamodb'
 region_name='ap-northeast-2'
 
-#calendar
-from dateutil.tz import gettz
-from dateutil import relativedelta
-import calendar
-from datetime import datetime,timedelta
-dateDict = {0: 'Mon', 1: 'Tue', 2: 'Wed', 3: 'Thu', 4: 'Fri', 5: 'Sat', 6: 'Sun'}
-
-#investpy
-import investpy
-
-
-aws_access_key_id=st.secrets['aws_access']
-aws_secret_access_key=st.secrets['aws_secret']
+aws_access_key_id="AKIA6IE5OZRJDUDS74NL"
+aws_secret_access_key="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"
+#aws_access_key_id=st.secrets['aws_access']
+#aws_secret_access_key=st.secrets['aws_secret']
 
 
 def binance_funding():
@@ -57,46 +49,14 @@ def neuralprophet():
     }
     return result
 
-def economic_calendar():
-    # Convert calendar
-    now = datetime.now(gettz('Asia/Seoul')).now()  # now를 GMT+9으로 호출
-    sat_now = now + relativedelta.relativedelta(weekday=calendar.SATURDAY)  # 이번주 토요일 호출
-    if now.day==sat_now.day:
-        sat_now+=timedelta(days=7)
-
-    # investpy에 변수 date 선언
-    from_date = now.strftime("%d/%m/%Y")
-    to_date = sat_now.strftime("%d/%m/%Y")
-
-    df = investpy.news.economic_calendar(time_zone='GMT +9:00',
-                                         time_filter='time_only',
-                                         countries=None,
-                                         importances=None,
-                                         categories=None,
-                                         from_date=from_date,
-                                         to_date=to_date)
-
-    data = df[(df['zone'] == 'united states') & (df['importance'].isin(['high','medium']))]
-    data = data[['date', 'time', 'event', 'actual', 'forecast', 'previous']].reset_index(drop=True)
-
-    data['date1'] = data['date'].apply(lambda x: x.split("/"))  # 날짜를 한국형으로 변환
-    data['date2'] = data['date1'].apply(lambda x: x[2] + "/" + x[1] + "/" + x[0])  # Date2에 날짜를 한국형으로 변환
-    data['date2'] = pd.to_datetime(data['date2'])
-    data['day'] = data['date2'].apply(lambda x: dateDict[x.weekday()])
-    data['date'] = data['date2'].astype(str) + ' ' + data['time'] + '(' + data['day'] + ')'
-    data = data[['date', 'event','actual', 'forecast', 'previous']]
-    data.set_index(keys=['date'], inplace=True, drop=True)
-    return data
-
-
 ###########
 
 st.title("Main : Market Monitoring")
 
-tab1, tab2, tab3, tab4 = st.tabs(["BTC Forecasting",
+tab1, tab2, tab3= st.tabs(["BTC Forecasting",
                             "Funding Rates",
-                            "Daily BTC Global Premium",
-                            "Economic Calendar"])
+                            "Daily BTC Global Premium"
+                            ])
 
 with tab1:
     st.header("BTC Forecasting(by neuralprophet)")
@@ -129,10 +89,7 @@ with tab3:
     st.write("< BASE PRICE : Binance "+premium[2]+ " ("+premium[1]+") >")
     st.table(premium[0].transpose())
 
-with tab4:
-    st.header("Economic Calendar")
-    calendar=economic_calendar()
-    st.table(calendar)
+
 
 
 
